9758b5ebae86a317732daa006dcce748f569578b
Mizuki <mizuki@Yota-KnoMacBook-Pro.local>
2026-02-14 16:08:50 +0900

    chore: build to docs

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,11 @@ export default defineConfig({
     },
   },
 
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
+
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ["**/*.svg", "**/*.csv"],
 });
